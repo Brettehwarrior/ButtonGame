01b3b52e6e30220c598032ba73ce1ed33a26d704
Trent <trentthebaker@gmail.com>
2022-02-27 13:37:11 -0400

    Bunch of sounds

---
+++ Assets/Scripts/SoundOnCollision.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace UnityTemplateProjects
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class SoundOnCollision : MonoBehaviour
+    {
+        [SerializeField] private AudioClip sound;
+
+        private AudioSource _audioSource;
+
+        private void Start()
+        {
+            _audioSource = GetComponent<AudioSource>();
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (collision.relativeVelocity.magnitude > 0.2f)
+                _audioSource.PlayOneShot(sound);
+        }
+    }
+}
