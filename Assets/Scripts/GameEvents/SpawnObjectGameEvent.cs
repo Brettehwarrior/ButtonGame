a53988d47cdca9be80237b9ce48bd38dd9429a1a
Trent <trentthebaker@gmail.com>
2022-02-27 01:01:04 -0400

    Random rotation on spawn object game event

--- Assets/Scripts/GameEvents/SpawnObjectGameEvent.cs
+++ Assets/Scripts/GameEvents/SpawnObjectGameEvent.cs
@@ -8,13 +8,21 @@ namespace GameEvents
         [SerializeField] private GameObject toSpawn;
         [SerializeField] private int min = 1;
         [SerializeField] private int max = 1;
+        [SerializeField] private bool randomRotation;
         private void Start()
         {
             int spawnCount = Random.Range(min, max + 1);
             for (int i = 0; i < spawnCount; i++)
             {
-                Instantiate(toSpawn, transform);
+                Quaternion rotation = transform.rotation;
+                if (randomRotation)
+                {
+                    rotation = Quaternion.Euler(0f, Random.Range(0, 359), 0f);
+                }
+                Instantiate(toSpawn, transform.position, rotation);
             }
+            
+            Destroy(gameObject);
         }
     }
 }
